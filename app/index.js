f61215b3cfa2aa3496aa30173bad56395edcd3f4
irminaj <irminaj@github.com>
2023-04-11 22:19:00 +0300

    add function to display the new book form by clicking the button

--- app/index.js
+++ app/index.js
@@ -1,1 +1,7 @@
+const bookForm = document.querySelector(".new_book_form");
+const addNewBookBtn = document.querySelector("#add_new_book");
 
+addNewBookBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  bookForm.style = "display:block";
+});
