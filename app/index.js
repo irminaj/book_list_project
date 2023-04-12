ed650183435eae223d40e4d33cfa43b2c5324f95
irminaj <irminaj@github.com>
2023-04-12 18:10:47 +0300

    add button to close the form

--- app/index.js
+++ app/index.js
@@ -1,7 +1,13 @@
 const bookForm = document.querySelector(".new_book_form");
 const addNewBookBtn = document.querySelector("#add_new_book");
+const closFormBtn = document.querySelector("#close_btn");
 
 addNewBookBtn.addEventListener("click", (e) => {
   e.preventDefault();
   bookForm.style = "display:block";
+});
+
+closFormBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  bookForm.style = "display: none";
 });
