dbdc534937424d58793b50fc2232c33cb49cf721
irminaj <irminaj@github.com>
2023-04-13 22:15:38 +0300

    add funtions to filter books by price

--- dist/index.1caaeb74.js
+++ dist/index.1caaeb74.js
@@ -2,6 +2,8 @@ const authorList = document.querySelector("#authors_list");
 const categoriesList = document.querySelector("#categories_list");
 const authorListArray = JSON.parse(localStorage.getItem("author-list"));
 const categoriesListArray = JSON.parse(localStorage.getItem("category-list"));
+const highestToLowestPrice = document.querySelector("#highest_to_lowest_price");
+const lowestTHighestPrice = document.querySelector("#lowest_to_highest_price");
 const authorListTemplate = (author)=>{
     return `
   <a onclick="filterAuthor(\`` + author + `\`)">${author}</a>`;
@@ -34,5 +36,35 @@ const filterCategory = (bookCategory)=>{
         if (book.category === bookCategory) booksContainer.innerHTML += bookTemplate(book);
     });
 };
+const highestToLowest = ()=>{
+    bookList.sort((a, b)=>{
+        return b.price - a.price;
+    });
+    booksContainer.innerHTML = "";
+    bookList.forEach((book)=>{
+        console.log(book);
+        booksContainer.innerHTML += bookTemplate(book);
+    });
+};
+highestToLowestPrice.addEventListener("click", (e)=>{
+    e.preventDefault();
+    console.log("Hello");
+    highestToLowest();
+});
+const lowestToHighest = ()=>{
+    bookList.sort((a, b)=>{
+        return a.price - b.price;
+    });
+    booksContainer.innerHTML = "";
+    bookList.forEach((book)=>{
+        console.log(book);
+        booksContainer.innerHTML += bookTemplate(book);
+    });
+};
+lowestTHighestPrice.addEventListener("click", (e)=>{
+    e.preventDefault();
+    console.log("Hello");
+    lowestToHighest();
+});
 
 //# sourceMappingURL=index.1caaeb74.js.map
