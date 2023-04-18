b9362a1f39be1767b449717e594570dc83e02de0
irminaj <irminaj@github.com>
2023-04-18 23:25:40 +0300

    fix search function to not case-sensitive

--- app/search.js
+++ app/search.js
@@ -2,13 +2,13 @@ const searchInput = document.querySelector("#search_input");
 const searchBtn = document.querySelector("#search_btn");
 
 const searchForBook = () => {
-  let searchValue = searchInput.value;
+  let searchValue = searchInput.value.toUpperCase();
   booksContainer.innerHTML = "";
   bookList.forEach((book) => {
     if (
-      book.author.includes(searchValue) ||
-      book.title.includes(searchValue) ||
-      book.category.includes(searchValue) ||
+      book.author.toUpperCase().includes(searchValue) ||
+      book.title.toUpperCase().includes(searchValue) ||
+      book.category.toUpperCase().includes(searchValue) ||
       book.year.includes(searchValue) ||
       book.price.includes(searchValue)
     ) {
